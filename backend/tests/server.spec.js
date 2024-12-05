943077aa6f233d908d7c8ebbc19278889b8bb07f
molivaresz <molivaresz1@gmail.com>
2024-12-05 00:01:20 -0300

    modified: test GET /producto/obtener_producto

--- backend/tests/server.spec.js
+++ backend/tests/server.spec.js
@@ -1,11 +1,10 @@
 const request = require("supertest");
 const server = require("../index")
 
-describe("Operaciones newfonda365", () => {
+describe("GET /fonda365/obtiene_comunas", () => {
     it("Obteniendo un 200 en obtiene_comunas", async () => {
-        const { body, statusCode}= await request(server).get("/fonda365/obtiene_comunas").send();
+        const { statusCode}= await request(server).get("/fonda365/obtiene_comunas").send();
         expect(statusCode).toBe(200);
-        expect(body).toBeInstanceOf(Array);
     });
 });
 
@@ -24,5 +23,13 @@ describe("POST /comentarios/obtener_comentario", () => {
         console.log(response.body)
         // Verifica el cuerpo de la respuesta
         expect(response.body).toBeInstanceOf(Array)
+    });
+});
+
+describe("GET /producto/obtener_producto", () => {
+    it("Obteniendo un 200 y valida el tipo de dato de body en obtener_producto", async () => {
+        const { body, statusCode}= await request(server).get("/producto/obtener_producto").send();
+        expect(statusCode).toBe(200);
+        expect(body).toBeInstanceOf(Array);
     });
 });
