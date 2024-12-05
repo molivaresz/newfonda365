87ac93c81ec20c2c688708fb2cbb18c7f8b0ae5f
rolandobrice <rolandojose1109@gamil.com>
2024-12-04 23:38:52 -0300

    modified: test POST /comentarios/obtener_comentario

--- backend/tests/server.spec.js
+++ backend/tests/server.spec.js
@@ -8,3 +8,21 @@ describe("Operaciones newfonda365", () => {
         expect(body).toBeInstanceOf(Array);
     });
 });
+
+describe("POST /comentarios/obtener_comentario", () => {
+    it("Debería retornar un 200 y los datos del comentario creado", async () => {
+        const payload = {
+            idproducto: 2
+        };
+
+        const response = await request(server)
+            .post("/comentarios/obtener_comentario")
+            .send(payload);
+
+        // Verifica el status
+        expect(response.statusCode).toBe(200);
+        console.log(response.body)
+        // Verifica el cuerpo de la respuesta
+        expect(response.body).toBeInstanceOf(Array)
+    });
+});
